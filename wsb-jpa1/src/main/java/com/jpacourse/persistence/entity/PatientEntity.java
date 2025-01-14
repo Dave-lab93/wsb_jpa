34153dc3853be4a4854aebb88bb446ea991672ec
Dave-lab93 <cj995@o2.pl>
2025-01-14 19:47:33 +0100

    Add files via upload

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
@@ -1,14 +1,10 @@
 package com.jpacourse.persistence.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import javax.persistence.*;
 @Entity
 @Table(name = "PATIENT")
 public class PatientEntity {
@@ -34,60 +30,35 @@ public class PatientEntity {
 	@Column(nullable = false)
 	private LocalDate dateOfBirth;
 
-	public Long getId() {
-		return id;
-	}
+	private Integer age;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<VisitEntity> visits;
 
-	public String getFirstName() {
-		return firstName;
-	}
+	public Long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String firstName) { this.firstName = firstName; }
 
-	public String getLastName() {
-		return lastName;
-	}
+	public String getLastName() { return lastName; }
+	public void setLastName(String lastName) { this.lastName = lastName; }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+	public String getTelephoneNumber() { return telephoneNumber; }
+	public void setTelephoneNumber(String telephoneNumber) { this.telephoneNumber = telephoneNumber; }
 
-	public String getTelephoneNumber() {
-		return telephoneNumber;
-	}
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
 
-	public void setTelephoneNumber(String telephoneNumber) {
-		this.telephoneNumber = telephoneNumber;
-	}
+	public String getPatientNumber() { return patientNumber; }
+	public void setPatientNumber(String patientNumber) { this.patientNumber = patientNumber; }
 
-	public String getEmail() {
-		return email;
-	}
+	public LocalDate getDateOfBirth() { return dateOfBirth; }
+	public void setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth; }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	public Integer getAge() { return age; }
+	public void setAge(Integer age) { this.age = age; }
 
-	public String getPatientNumber() {
-		return patientNumber;
-	}
-
-	public void setPatientNumber(String patientNumber) {
-		this.patientNumber = patientNumber;
-	}
-
-	public LocalDate getDateOfBirth() {
-		return dateOfBirth;
-	}
-
-	public void setDateOfBirth(LocalDate dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-
+	public List<VisitEntity> getVisits() { return visits; }
+	public void setVisits(List<VisitEntity> visits) { this.visits = visits; }
 }
