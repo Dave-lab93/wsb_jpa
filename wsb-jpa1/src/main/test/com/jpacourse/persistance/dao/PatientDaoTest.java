e0a0bb24aa31142ca904f601d14fee74efe1e604
Dave-lab93 <cj995@o2.pl>
2025-01-24 19:41:46 +0100

    poprawa
    poprawa

--- wsb-jpa1/src/main/test/com/jpacourse/persistance/dao/PatientDaoTest.java
+++ wsb-jpa1/src/main/test/com/jpacourse/persistance/dao/PatientDaoTest.java
@@ -1,46 +1,30 @@
 package com.jpacourse.persistance.dao;
 
 import com.jpacourse.persistence.dao.PatientDao;
-import com.jpacourse.persistence.entity.DrugEntity;
 import com.jpacourse.persistence.entity.PatientEntity;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@DataJpaTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PatientDaoTest {
 
     @Autowired
     private PatientDao patientDao;
-    @Repository
-    public interface PatientDao extends JpaRepository<PatientEntity, Long> {
-        List<PatientEntity> findByDrug(String drugName);
-    }
 
     @Test
     @Transactional
     public void testFindByDrugName() {
-
-        PatientEntity patient = new PatientEntity();
-        patient.setFirstName("Jan");
-        patient.setLastName("Kowalski");
-
-        DrugEntity drug = new DrugEntity();
-        drug.setName("Ibuprom");
-
-        List<DrugEntity> drugs = new ArrayList<>();
-        drugs.add(drug);
-        patient.setDrugs(drugs);
-        patientDao.save(patient);
-
+        
         String drugName = "Ibuprom";
         List<PatientEntity> patients = patientDao.findByDrug(drugName);
 
